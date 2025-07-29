72cbfadfdce7ad900e1bade1c19f3c81bc4577ff
USER <tamimachy9897@gmail.com>
2025-07-29 21:56:53 +0600

    Add project files.

---
+++ StartExplore.API/Data/StartExploreDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StartExploreAPI.Models.Domain;
+
+namespace StartExploreAPI.Data
+{
+    public class StartExploreDbContext : DbContext
+    {
+        public StartExploreDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
---
+++ StartExplore.API/Models/Domain/Difficulty.cs
@@ -0,0 +1,8 @@
+﻿namespace StartExploreAPI.Models.Domain
+{
+    public class Difficulty
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
