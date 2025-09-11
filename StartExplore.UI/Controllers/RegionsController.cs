9b19ebd8262b46c860e1e268d76a65ccd9ceae4a
USER <tamimachy9897@gmail.com>
2025-09-11 23:07:58 +0600

    post method added.

--- StartExplore.UI/Controllers/RegionsController.cs
+++ StartExplore.UI/Controllers/RegionsController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StartExplore.UI.Models;
 using StartExplore.UI.Models.DTO;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace StartExplore.UI.Controllers
@@ -12,6 +15,7 @@ namespace StartExplore.UI.Controllers
         {
             this.httpClientFactory = httpClientFactory;
         }
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             List<RegionDto> response = new List<RegionDto>();
@@ -23,17 +27,43 @@ namespace StartExplore.UI.Controllers
                 var httpResponseMessage = await client.GetAsync("https://localhost:7223/api/regions");
 
                 httpResponseMessage.EnsureSuccessStatusCode();
-                
+
                 response.AddRange(await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<RegionDto>>());
-                
+
             }
             catch (Exception ex)
             {
                 // Log the exception
-                
+
             }
 
             return View(response);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(AddRegionViewModel model)
+        {
+            var client = httpClientFactory.CreateClient();
+            var httpRequestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://localhost:7223/api/regions"),
+                Content = new StringContent(JsonSerializer.Serialize(model), Encoding.UTF8, "application/json")
+            };
+            var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+            httpResponseMessage.EnsureSuccessStatusCode();  
+
+            var response = await httpResponseMessage.Content.ReadFromJsonAsync<RegionDto>();
+            if(response is not null)
+            {
+                return RedirectToAction("Index", "Regions");
+            }
+            return View(model);
         }
     }
 } 
